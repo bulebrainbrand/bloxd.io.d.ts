42007c30ad22a9a490209afc99feffa2d6eadf3d
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-03-28 22:00:19 +0900

    add deleteShopItem

--- api-type-def/normal/shops/index.d.ts
+++ api-type-def/normal/shops/index.d.ts
@@ -1,6 +1,8 @@
 import ShowShopTutorialInterface from "./showShopTutorial";
 import CreateShopItemInterface from "./createShopItem";
 import UpdateShopItemInterface from "./updateShopItem";
+import DeleteShopItemInterface from "./deleteShopItem";
 export type ShopApis = ShowShopTutorialInterface &
   CreateShopItemInterface &
-  UpdateShopItemInterface;
+  UpdateShopItemInterface &
+  DeleteShopItemInterface;

01416193b0fd8e02f4e5d34540b6640fca5bd1f6
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-03-29 13:15:47 +0900

    add SendTopRightHelper

--- src/api-type-def/normal/message/index.ts
+++ src/api-type-def/normal/message/index.ts
@@ -1,6 +1,8 @@
 import BroadcastMessageInterface from "./broadcastMessage";
 import SendMessageInterface from "./sendMessage";
 import SendFlyingMiddleMessage from "./sendFlyingMiddleMessage";
+import SendTopRightHelper from "./sendTopRightHelper";
 export type MessageApis = BroadcastMessageInterface &
   SendMessageInterface &
-  SendFlyingMiddleMessage;
+  SendFlyingMiddleMessage &
+  SendTopRightHelper;

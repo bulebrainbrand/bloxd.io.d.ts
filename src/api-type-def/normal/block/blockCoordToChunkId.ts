123099c8c9fcf5c36665ef371e5e33b57adac79f
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-03-30 09:13:11 +0900

    add some jsdoc

--- src/api-type-def/normal/block/blockCoordToChunkId.ts
+++ src/api-type-def/normal/block/blockCoordToChunkId.ts
@@ -1,5 +1,12 @@
 import { ChunkId, Coordinate } from "@type";
-
+/**
+ * Get the unique id of the chunk containing pos in the current map
+ *
+ * CunnkId is like a `5|5|5` string
+ *
+ * @param {number[]} pos
+ * @returns {ChunkId}
+ */
 declare const blockCoordToChunkId: (pos: Coordinate) => ChunkId;
 
 export { blockCoordToChunkId };

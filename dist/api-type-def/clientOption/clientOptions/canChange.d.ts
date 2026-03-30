1b796ad31d9c09a1e8d9bb27cf491f3ab27c016f
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-03-30 14:21:53 +0900

    fix typo

--- dist/api-type-def/clientOption/clientOptions/canChange.d.ts
+++ dist/api-type-def/clientOption/clientOptions/canChange.d.ts
@@ -6,70 +6,70 @@ import { PlayerId } from "../../../type/index";
  */
 type ValueType = boolean;
 interface SetClientOption {
-    /**
-     * set status player who can changing block
-     *
-     * option docment:
-     * Whether the player can change blocks
-     *
-     * @overload
-     *
-     * @param {PlayerId} playerId
-     * @param {"canchange"} option
-     * @param {true} value
-     */
-    setClientOption(playerId: PlayerId, option: "canChange", value: true): void;
-    /**
-     * set status player who can not changing block
-     *
-     * option docment:
-     * Whether the player can change blocks
-     *
-     * @overload
-     *
-     * @param {PlayerId} playerId
-     * @param {"canchange"} option
-     * @param {false} value
-     * @returns {void}
-     */
-    setClientOption(playerId: PlayerId, option: "canChange", value: false): void;
+  /**
+   * set status player who can changing block
+   *
+   * option document:
+   * Whether the player can change blocks
+   *
+   * @overload
+   *
+   * @param {PlayerId} playerId
+   * @param {"canchange"} option
+   * @param {true} value
+   */
+  setClientOption(playerId: PlayerId, option: "canChange", value: true): void;
+  /**
+   * set status player who can not changing block
+   *
+   * option document:
+   * Whether the player can change blocks
+   *
+   * @overload
+   *
+   * @param {PlayerId} playerId
+   * @param {"canchange"} option
+   * @param {false} value
+   * @returns {void}
+   */
+  setClientOption(playerId: PlayerId, option: "canChange", value: false): void;
 }
 interface GetClientOption {
-    /**
-     * get player can change block
-     *
-     * option docment:
-     * Whether the player can change blocks
-     *
-     * @overload
-     *
-     * @param {PlayerId} playerId
-     * @param {"canChange"} option
-     * @returns {boolean}
-     */
-    getClientOption(playerId: PlayerId, option: "canChange"): boolean;
+  /**
+   * get player can change block
+   *
+   * option document:
+   * Whether the player can change blocks
+   *
+   * @overload
+   *
+   * @param {PlayerId} playerId
+   * @param {"canChange"} option
+   * @returns {boolean}
+   */
+  getClientOption(playerId: PlayerId, option: "canChange"): boolean;
 }
 interface SetClientOptionToDefault {
-    /**
-     * set canChange option to default
-     * canChange default is `true`
-     *
-     * option docment:
-     * Whether the player can change blocks
-     *
-     * @overload
-     *
-     * @param {PlayerId} playerId
-     * @param {"canChange"} option
-     * @returns {void}
-     */
-    setClientOptionToDefault(playerId: PlayerId, option: "canChange"): void;
+  /**
+   * set canChange option to default
+   * canChange default is `true`
+   *
+   * option document:
+   * Whether the player can change blocks
+   *
+   * @overload
+   *
+   * @param {PlayerId} playerId
+   * @param {"canChange"} option
+   * @returns {void}
+   */
+  setClientOptionToDefault(playerId: PlayerId, option: "canChange"): void;
 }
 type ExportTypes = {
-    SetClientOption: SetClientOption;
-    SetClientOptionToDefault: SetClientOptionToDefault;
-    GetClientOption: GetClientOption;
-    name: "canChange";
-    ValueType: ValueType;
+  SetClientOption: SetClientOption;
+  SetClientOptionToDefault: SetClientOptionToDefault;
+  GetClientOption: GetClientOption;
+  name: "canChange";
+  ValueType: ValueType;
 };
 export default ExportTypes;

968ea131651a83a2657892f7662f1052312301a2
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-01 12:31:55 +0900

    move file

--- src/api-type-def/normal/index.ts
+++ src/api-type-def/normal/index.ts
@@ -1,6 +1,6 @@
-import { getUnitCoordinatesLifeformWithin } from "./getUnitCoordinatesLifeformWithin";
+import { getUnitCoordinatesLifeformWithin } from "./entity/getUnitCoordinatesLifeformWithin";
 import { playParticleEffect } from "./playParticleEffect";
-import { forceRespawn } from "./forceRespawn";
+import { forceRespawn } from "./player/forceRespawn";
 import { ShopApis } from "./shops/index";
 import { ShieldApis } from "./shields/index";
 import { PositionApis } from "./positions/index";
@@ -11,7 +11,7 @@ import { KillstreakApis } from "./killstreak/index";
 import { MessageApis } from "./message/index";
 import { PlayerApis } from "./player/index";
 import { EntityApis } from "./entity/index";
-import { now } from "./now";
+import { now } from "./util/now";
 import { SetCanChangeApis } from "./canChangeBlockOption/index";
 import { SetWalkOptionsApis } from "./setWalkOptions/index";
 import { ChestApis } from "./chest/index";

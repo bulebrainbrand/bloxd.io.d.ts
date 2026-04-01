748e6d140c0eb3838fb447596d6b67851e1e8f2f
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 06:27:00 +0900

    add setEntityHeading

--- src/api-type-def/normal/entity/setEntityHeading.ts
+++ src/api-type-def/normal/entity/setEntityHeading.ts
@@ -1,4 +1,19 @@
-// TODO: make this api
+import { EntityId, Radian } from "@type";
+
+/**
+ * @deprecated use setEntityRotation
+ * Set the heading for a server-auth entity.
+ *
+ * @param {EntityId} entityId
+ * @param {number} newHeading
+ * @returns {void}
+ */
+declare const setEntityHeading: (
+  entityId: EntityId,
+  newHeading: Radian,
+) => void;
+
+export { setEntityHeading };
 
 /**
  * @deprecated use setEntityRotation

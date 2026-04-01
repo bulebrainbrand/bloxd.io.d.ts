8fb7a318e9d01794e3213e66a411da2469dce665
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 06:19:04 +0900

    add getEntitiesInRect

--- src/api-type-def/normal/entity/getEntitiesInRect.ts
+++ src/api-type-def/normal/entity/getEntitiesInRect.ts
@@ -1,4 +1,18 @@
-// TODO: make this api
+import { Coordinate, EntityId } from "@type";
+
+/**
+ * Get the entities in the rect between [minX, minY, minZ] and [maxX, maxY, maxZ]
+ *
+ * @param {number[]} minCoords
+ * @param {number[]} maxCoords
+ * @returns {EntityId[]}
+ */
+declare const getEntitiesInRect: (
+  minCoords: Coordinate,
+  maxCoords: Coordinate,
+) => EntityId[];
+export { getEntitiesInRect };
+
 /**
  * Get the entities in the rect between [minX, minY, minZ] and [maxX, maxY, maxZ]
  *

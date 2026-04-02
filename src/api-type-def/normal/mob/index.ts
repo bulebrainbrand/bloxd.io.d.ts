bb4941686b9ee1d702a6a823451df3996bfeef72
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:32:44 +0900

    marge index.ts

---
+++ src/api-type-def/normal/mob/index.ts
@@ -0,0 +1,17 @@
+import { attemptSpawnMob } from "./attemptSpawnMob";
+import { createMobHerd } from "./createMobHerd";
+import { despawnMob } from "./despawnMob";
+import { getMobAiState } from "./getMobAiState";
+import { getMobIds } from "./getMobIds";
+import { getNumMobs } from "./getNumMobs";
+import { setMobAiState } from "./setMobAiState";
+
+export const MobApis = {
+  attemptSpawnMob,
+  createMobHerd,
+  despawnMob,
+  getMobAiState,
+  getMobIds,
+  getNumMobs,
+  setMobAiState,
+};

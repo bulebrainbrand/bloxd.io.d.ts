d1b7f34174e55b1cf96c233995c616a69df48e7a
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:42:32 +0900

    add getPlayerFacingInfo

--- src/api-type-def/normal/player/getPlayerFacingInfo.ts
+++ src/api-type-def/normal/player/getPlayerFacingInfo.ts
@@ -1,4 +1,20 @@
-// TODO: make this api
+import { AngleDir, Coordinate, Direcrion, PlayerId } from "@type";
+
+/**
+ * Get the position of a player's camera and the direction (both in Euclidean and spherical coordinates) they are attempting to use an item.
+ * The camPos has the same limitations described in getPlayerTargetInfo
+ *
+ * @param {PlayerId} playerId
+ * @returns { { camPos: [number, number, number]; dir: [number, number, number]; angleDir: AngleDir; moveHeading: number } }
+ */
+declare const getPlayerFacingInfo: (playerId: PlayerId) => {
+  camPos: Coordinate;
+  dir: Direcrion;
+  angleDir: AngleDir;
+  moveHeading: number;
+};
+
+export { getPlayerFacingInfo };
 
 /**
  * Get the position of a player's camera and the direction (both in Euclidean and spherical coordinates) they are attempting to use an item.

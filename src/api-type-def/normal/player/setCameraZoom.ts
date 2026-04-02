4090f850178ddfba1b535b8ade5df19aefe71146
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:43:31 +0900

    add setCameraZoom

--- src/api-type-def/normal/player/setCameraZoom.ts
+++ src/api-type-def/normal/player/setCameraZoom.ts
@@ -1,5 +1,16 @@
-// TODO: make this api
+import { PlayerId } from "@type";
 
+/**
+ * Set camera zoom for a player
+ *
+ * @param {PlayerId} playerId
+ * @param {number} zoom
+ * @returns {void}
+ *
+ * setCameraZoom(playerId, zoom);
+ */
+declare const setCameraZoom: (playerId: PlayerId, zoom: number) => void;
+export { setCameraZoom };
 /**
  * Set camera zoom for a player
  *

de697ce4a8d00d3ddb7ce16c44eed4b30151561c
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:54:22 +0900

    add addFollowingEntityToPlayer

--- src/api-type-def/normal/player/addFollowingEntityToPlayer.ts
+++ src/api-type-def/normal/player/addFollowingEntityToPlayer.ts
@@ -1,4 +1,21 @@
-// TODO: make this api
+import { Coordinate, EntityId, PlayerId } from "@type";
+
+/**
+ * Add following entity to player
+ *
+ * @param {PlayerId} playerId
+ * @param {EntityId} eId
+ * @param {number[]} [offset]
+ * @param {boolean} [followsPlayerRotation]
+ * @returns {void}
+ */
+declare const addFollowingEntityToPlayer: (
+  playerId: PlayerId,
+  eId: EntityId,
+  offset?: Coordinate,
+  followsPlayerRotation?: boolean,
+) => void;
+export { addFollowingEntityToPlayer };
 
 /**
  * Add following entity to player

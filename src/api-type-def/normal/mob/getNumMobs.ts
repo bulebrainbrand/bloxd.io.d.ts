5bd9f5e57dfcd711b56ecf53947a3759a2577d76
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:20:11 +0900

    add getNumMobs

--- src/api-type-def/normal/mob/getNumMobs.ts
+++ src/api-type-def/normal/mob/getNumMobs.ts
@@ -1,4 +1,10 @@
-// TODO: make this api
+/**
+ * Get the number of mobs in the world.
+ *
+ * @returns {number}
+ */
+declare const getNumMobs: () => number;
+export { getNumMobs };
 
 /**
  * Get the number of mobs in the world.

c5c82f894191a0ac694ff2b9bf8b5cfb90d07049
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:17:54 +0900

    add despawnMob

--- src/api-type-def/normal/mob/despawnMob.ts
+++ src/api-type-def/normal/mob/despawnMob.ts
@@ -1,4 +1,16 @@
-// TODO: make this api
+import { MobId } from "@type";
+
+/**
+ * Dispose of a mob's state and remove them from the world without triggering "on death" flows.
+ * Always succeeds.
+ *
+ * @param {MobId} mobId
+ * @returns {void}
+ *
+ * despawnMob(mobId);
+ */
+declare const despawnMob: (mobId: MobId) => void;
+export { despawnMob };
 
 /**
  * Dispose of a mob's state and remove them from the world without triggering "on death" flows.

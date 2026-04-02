e00f98f299e554baccd8a57c48ade9fcb57f1e84
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-02 12:55:09 +0900

    add removeMiddleScreenBar

--- src/api-type-def/normal/player/removeMiddleScreenBar.ts
+++ src/api-type-def/normal/player/removeMiddleScreenBar.ts
@@ -1,5 +1,13 @@
-// TODO: make this api
+import { PlayerId } from "@type";
 
+/**
+ * If there is any current middle screen bar running, this will hide it
+ *
+ * @param {PlayerId} playerId
+ * @returns {void}
+ */
+declare const removeMiddleScreenBar: (playerId: PlayerId) => void;
+export { removeMiddleScreenBar };
 /**
  * If there is any current middle screen bar running, this will hide it
  *

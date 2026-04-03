c55fa6bcd502b9d19391f12393cdfe0ff0bd49c9
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-04-03 11:54:10 +0900

    add last apis

--- src/api-type-def/normal/item/getItemStat.ts
+++ src/api-type-def/normal/item/getItemStat.ts
@@ -1,5 +1,23 @@
-// TODO: make this api
-
+import { AnyMetadataItem, ItemName, LifeformId } from "@type";
+// TODO: make more better
+/**
+ * Get stat info about a block or item
+ * Either based on a client option for a player: (e.g. `DirtTtb`)
+ * or its entry in blockMetadata.ts or nonBlockMetadata in itemMetadata.ts if no client option is set.
+ *
+ * If null is passed for lifeformId, this is simply its entry in blockMetadata etc.
+ *
+ * @param {PNull<LifeformId>} lifeformId
+ * @param {ItemName} itemName
+ * @param {K} stat
+ * @returns {AnyMetadataItem[K]}
+ */
+declare const getItemStat: (
+  lifeformId: LifeformId | null,
+  itemName: ItemName,
+  stat: string,
+) => AnyMetadataItem;
+export { getItemStat };
 /**
  * Get stat info about a block or item
  * Either based on a client option for a player: (e.g. `DirtTtb`)
